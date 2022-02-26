07c1fa4f8efcb61a5e3ec66267c06882ddc06508
Sebastian Mynarski <smynarski89@gmail.com>
2022-02-26 08:40:20 +0100

    Start work on the backend

---
+++ script.js
@@ -0,0 +1,14 @@
+function setCurrentDate() {
+
+    let currentDate = new Date();
+
+    let day = currentDate.getDate();
+    if (day < 10) day = `0${day}`;
+
+    let month = currentDate.getMonth() + 1;
+    if (month < 10) month = `0${month}`;
+
+    let today = currentDate.getFullYear() + `-${month}-${day}`;
+
+    document.querySelector('#inputExpenceDate').value = today;
+}
